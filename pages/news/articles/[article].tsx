69638fee265b7209c89bc9d3336c32324a0d0e1b
Yemelyanov <nikolaef.post@gmail.com>
2023-01-06 13:36:29 +0200

    feat: mobile ProgramRoom2

--- pages/news/articles/[article].tsx
+++ pages/news/articles/[article].tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { GetStaticPaths, GetStaticProps } from "next/types";
 import React from "react";
 import { AiOutlineCalendar } from "react-icons/ai";
+
+import styles from "@/../styles/componentsStyle/News.module.scss";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import {
@@ -20,8 +22,6 @@ import client from "../../../contentfulClient/index";
 import { useLanguage } from "../../../context";
 import { useMediaQuery } from "../../../helpers/useMedia";
 import ticket from "../../../public/News/news_ticket.png";
-
-import styles from "@/../styles/componentsStyle/News.module.scss";
 
 interface IArticle {
   data: INewArticle;
